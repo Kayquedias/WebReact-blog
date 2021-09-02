be7ddc80714259158e8e9d8b2aa3b530ea117b7a
Kayque Dias <kayquedias204@gmail.com>
2021-09-01 21:18:39 -0300

    reacting

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,14 @@
+const NavigationBar = () => {
+    return (
+      <nav className="navigation-top">
+        <h1>The Web Blog</h1>
+        
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default NavigationBar;
