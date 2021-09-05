fe34a7117dff3b98962c08d4747a7c1ae1143ab6
Kayque Dias <kayquedias204@gmail.com>
2021-09-04 22:15:10 -0300

    CSS update

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 
 const BLogDetails = () => {
     const { id } = useParams()
+    const { data: blog, isPending, error} = useFetch('http://localhost:8000/blogs/' + id)
 
     return (
         <div className="blog-detail">
-            <h2>Blog information - { id }</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{ error }</div>}
+            {blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                </article>
+            )}
         </div>
     );
 }
