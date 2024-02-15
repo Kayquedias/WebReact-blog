243179a5c31f0346379b4639ae48d5d499e01bfd
Kayquedias <kayquedias204@gmail.com>
2024-02-15 11:45:31 -0300

    change: translate not found message to pt-br

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
-import useFetch from '../hooks/useFetch';
+import useFetch from "../hooks/useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-  const { data: blogs, isLoaded, error } = useFetch('http://localhost:8000/blogs')
+  const {
+    data: blogs,
+    isLoaded,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   // Os dois pontos depois da variável data significa:
   // Seu nome será referido como blogs a partir de agora
+
   return (
     <div className="home">
       <h2>Trending Blogs</h2>
@@ -15,6 +19,6 @@ const Home = () => {
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
-}
+};
 
 export default Home;
--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
-    return (
-        <div className="unknown-page">
-            <h2>Sorry, page not found /:</h2>
-            <p>We could not access the requested page.</p>
-            <Link to="/">Return to the main page</Link>
-        </div>
-    );
-}
- 
+  return (
+    <div className="unknown-page">
+      <h2>Desculpe, Página não encontrada /:</h2>
+      <p>Nós não podemos acessar a página requisitada.</p>
+      <Link to="/">Retorne para página principal</Link>
+    </div>
+  );
+};
+
 export default NotFoundPage;
